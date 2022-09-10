b98a817fc5985dcedb959b007fab30f37025b4cf
MISS <basilio.stephanie@auf.edu.ph>
2022-09-10 15:17:21 +0800

    second commit

--- register.php
+++ register.php
@@ -1,6 +1,12 @@
 <html>  
 <head>   
-<title> Registration Form </title>  
+<title> Registration Form </title>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </head>  
 <body>
 
@@ -17,7 +23,7 @@
 <label> Picture </label>         
 <input type="file" name="picture" required /> <br> <br>
 
-<input type="submit" value="Submit Registration"/>
+<input type="submit" class="btn btn-secondary btn-lg" value="Submit Registration"/>
 </form>
  
     
